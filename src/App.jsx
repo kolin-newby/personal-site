aa9159de4eb4c25afea176d68ce3895e0ad28736
Kolin Newby <kolin.newby@gmail.com>
2025-11-03 12:44:17 -0700

    Removed console log

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,6 @@ const App = () => {
   }
 
   const isTouchDevice = useCallback(() => {
-    console.log(
-      "ontouchstart" in window ||
-        navigator.maxTouchPoints > 0 ||
-        navigator.msMaxTouchPoints > 0
-    );
     return (
       "ontouchstart" in window ||
       navigator.maxTouchPoints > 0 ||
